c2aebe126fa71cbe3b36f44f9813bd85479373d8
Razawaky <viniciusduarte564@gmail.com>
2023-03-26 21:36:29 -0300

    termino

--- js/imagens.js
+++ js/imagens.js
@@ -1,3 +1,26 @@
-function clicar(texto) {
-    alert(texto)
-}
+$(document).ready(function() {
+    //selecionar todos os buttons e monitorar os clicks
+$('.img-fluid').click(function(e){  //$ simplificação do getElementBy por causa do Jquery. Jquery tem que ler toda a documentação
+e.preventDefault() //Serve para controlar e gerenciar uma funcionaliade HTML (Uma função do Jquery)
+
+//capturar o ID do button clicado
+var ID = $(this).attr('id')
+console.log(ID)
+
+switch(ID) {
+case 'raposa':
+$('audio').attr('src', 'img/raposa.mp3')
+alert('ran ran ran rrrrrrr')
+break
+case 'dragon':
+$('audio').attr('src', 'img/dragao.mp3')
+alert('roar, roooor')
+break
+case 'corv':
+$('audio').attr('src', 'img/corvo.mp3')
+alert('croac croac, cuá')
+break
+}  
+
+})
+})
